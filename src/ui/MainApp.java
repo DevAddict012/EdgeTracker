0c1a359e6853c005446b5f272467836836770409
Letlhogonolo Setati <thomassetati0@gmail.com>
2026-04-03 21:43:31 +0200

    Updating my UI for adding a trade

--- src/ui/MainApp.java
+++ src/ui/MainApp.java
@@ -1,4 +1,5 @@
 package ui;
+//IMPORT THE BACKEND CLASSES
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -6,21 +7,58 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Welcome to the Edge Tracker");
 
-        Label pairLabel=new Label("Pair: ");
-        TextField pairField=new TextField();
+        Label heading = new Label("TradeJournal");
 
-        VBox root = new VBox();
-        root.getChildren().add(label);
+        Label pairLabel = new Label("Pair:");
+        TextField pairField = new TextField();
 
+        Label entryLabel = new Label("Entry Price:");
+        TextField entryField = new TextField();
 
-        Scene scene = new Scene(root, 400, 300);
+        Label directionLabel = new Label("Direction:");
+        ComboBox<String> directionField = new ComboBox<>();
+        directionField.getItems().addAll("BUY","SELL");
+
+        Label stopLossLabel = new Label("Stop Loss:");
+        TextField stopLossField = new TextField();
+
+        Label takeProfitLabel = new Label("Take Profit:");
+        TextField takeProfitField = new TextField();
+
+        Label resultLabel = new Label("Result:");
+        ComboBox<String> resultBox = new ComboBox<>();
+        resultBox.getItems().addAll("WIN", "LOSS");
+
+        Label setupLabel = new Label("Setup:");
+        TextField setup=new TextField();
+
+        Label dateLabel = new Label("Trade Date:");
+        DatePicker datePicker = new DatePicker();
+
+        Button addButton = new Button("Add Trade");
+
+        VBox root = new VBox(10);
+        root.getChildren().addAll(
+                heading,
+                pairLabel, pairField,
+                entryLabel, entryField,
+                directionLabel,directionField,
+                stopLossLabel, stopLossField,
+                takeProfitLabel, takeProfitField,
+                resultLabel, resultBox,
+                setupLabel, setup,
+                dateLabel, datePicker,
+                addButton
+        );
+
+        Scene scene = new Scene(root, 400, 700);
 
         stage.setTitle("Edge Tracker");
         stage.setScene(scene);

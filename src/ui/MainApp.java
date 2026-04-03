8514437fa9bc7d0e3fc27982bf78e908fd8c124b
Letlhogonolo Setati <thomassetati0@gmail.com>
2026-04-03 21:09:56 +0200

    Adding a Stage

--- src/ui/MainApp.java
+++ src/ui/MainApp.java
@@ -3,6 +3,7 @@ package ui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,14 +11,18 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hello, TradeJournal");
+        Label label = new Label("Welcome to the Edge Tracker");
+
+        Label pairLabel=new Label("Pair: ");
+        TextField pairField=new TextField();
 
         VBox root = new VBox();
         root.getChildren().add(label);
 
+
         Scene scene = new Scene(root, 400, 300);
 
-        stage.setTitle("TradeJournal");
+        stage.setTitle("Edge Tracker");
         stage.setScene(scene);
         stage.show();
     }
